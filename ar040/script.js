a6a1d879cf29624ed5d60ae2569865998a0ba2be
abdulrahmaannhh <abdulrahmaannhh@gmail.com>
2025-07-01 14:19:11 +0530

    JavaScript Introduction

--- ar040/script.js
+++ ar040/script.js
@@ -4,4 +4,11 @@ console.log("JavaScript is also running!");
 console.log("JavaScript is running like a charm!");
 
 var a = prompt("Enter a number:");
+var istrue = confirm("Is this true? (yes/no)");
+if (istrue) {
+    console.log("You answered yes.");
+}
+else {
+    console.log("You answered no.");
+}
 console.log("You entered: " + a);

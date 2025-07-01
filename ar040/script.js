aae0faff05ed8b87584b4d641aeb4400223e7c65
abdulrahmaannhh <abdulrahmaannhh@gmail.com>
2025-07-01 13:00:42 +0530

    JavaScript Introduction

--- ar040/script.js
+++ ar040/script.js
@@ -1,1 +1,7 @@
 alert("Welcome to JavaScript!");                    
+console.log("JavaScript is running!");
+console.log("JavaScript is also running!");
+console.log("JavaScript is running like a charm!");
+
+var a = prompt("Enter a number:");
+console.log("You entered: " + a);

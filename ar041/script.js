8396e4e93109a7a98822e9efe54e6578f43cbab8
abdulrahmaannhh <abdulrahmaannhh@gmail.com>
2025-07-04 18:15:42 +0530

    JavaScript Variables practice completed

--- ar041/script.js
+++ ar041/script.js
@@ -1,6 +1,40 @@
 console.log("Hey this is JS")
 
-var a = 5; 
-var b = 6; 
+let a = 5; 
+a= a + 7; // Reassigning value to 'a'
+let b = 6; 
+let c ="AR "
+let _a="Abdul Rahman"
+// var 55a= "Rahman" // Not allowed as variable name cannot start with a number
 
-console.log(a+b)
+// console.log(a+b+8)
+// console.log(typeof a, typeof b, typeof c)
+
+{
+    let a=66;
+    console.log(a)
+}
+console.log(a)
+
+const a1= 6;    
+// a1= a1 +1;Not allowed as a1 is a constant
+let x= "abdul rahman";
+let y= "22";
+let z=3.55;
+const p= true;
+let q= undefined
+let r= null;
+
+console.log(x, y, z, p, q, r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+let o=   {
+    name: "Abdul Rahman",
+    age: 24,
+"is_handsome":true,
+}
+console.log(o);
+o.sallary = "1000$";
+console.log(o);
+o.sallary = "2000$";
+console.log(o);

5f974daf6542530c5609f01929db3e69ea786d4e
abdulrahmaannhh <abdulrahmaannhh@gmail.com>
2025-07-05 11:53:20 +0530

    JavaScript conditional tutorial

---
+++ ar042/index.js
@@ -0,0 +1,15 @@
+console.log("Hello, I am conditional tutorial");
+
+let age = 17;
+let grace = 2;
+console.log(age + grace);
+console.log(age - grace);
+console.log(age * grace);
+console.log(age / grace);
+console.log(age ** grace);
+console.log(age % grace);
+if ((age + grace) >= 18) {
+    console.log("You are an adult.");
+} else {
+    console.log("You are a minor.");
+} 

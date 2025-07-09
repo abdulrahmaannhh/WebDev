e1f2987028ae124607bccd884a67154c43d3ca0c
abdulrahmaannhh <abdulrahmaannhh@gmail.com>
2025-07-09 23:30:56 +0530

    JavaScript loops

--- ar043/index.js
+++ ar043/index.js
@@ -3,3 +3,20 @@ let a =1;
 console.log(a)
 console.log(a+1)
 console.log(a+2)
+
+for (let i = 0; i < 100; i++) {
+    console.log(a+i);
+}
+
+let obj ={
+    name: "Abddul Rahman",
+    role: "Web Developer",
+    company: "Tech Solutions", 
+}
+
+for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        const element = obj[key];
+        
+    }
+}

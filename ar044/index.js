e0feae7ab2e202334deb0b25127eb6acf425bc80
abdulrahmaannhh <abdulrahmaannhh@gmail.com>
2025-07-10 21:50:07 +0530

    JavaScript function completed

--- ar044/index.js
+++ ar044/index.js
@@ -4,10 +4,33 @@
 // console.log("Hey AR, I think you need to check the logic in your code.");
 
 function nice(name) {
-    console.log("Hey "+name+" This is a test file for the ar044 module.")
-    console.log("Hey "+name+" are you sure this is the right file?")
-    console.log("Hey "+name+" your code is not working properly, please fix it.")
-    console.log("Hey "+name+" I think you need to check the logic in your code.")
+    console.log("Hey " + name + " This is a test file for the ar044 module.")
+    console.log("Hey " + name + " are you sure this is the right file?")
+    console.log("Hey " + name + " your code is not working properly, please fix it.")
+    console.log("Hey " + name + " I think you need to check the logic in your code.")
 }
 
 nice("AR");
+
+nice("Abdul Rahman");
+
+function sum(a, b, c = 3) {
+    // console.log(a+b);
+    console.log(a,b,c);
+    return a + b + c;
+}
+
+result1 = sum(5,5);
+result2 = sum(51, 10);
+result3 = sum(5, 160,5);
+console.log("The sum of these numbers is: ", result1)
+console.log("The sum of these numbers is: ", result2)
+console.log("The sum of these numbers is: ", result3)
+
+const func1 = (x)=> {
+    console.log("I am an arrow function", x);
+}
+
+func1(34)
+func1(66)
+func1(84)

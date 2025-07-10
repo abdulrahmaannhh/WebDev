d1215c3af5bb8f04faae44f8954892a51ea4b897
abdulrahmaannhh <abdulrahmaannhh@gmail.com>
2025-07-10 17:30:32 +0530

    JavaScript loops completed

--- ar043/index.js
+++ ar043/index.js
@@ -1,22 +1,36 @@
 console.log("I am a tutorial for Loops in JavaScript");
 let a =1;
-console.log(a)
-console.log(a+1)
-console.log(a+2)
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
 
-for (let i = 0; i < 100; i++) {
-    console.log(a+i);
-}
+// for (let i = 0; i < 100; i++) {
+//     console.log(a+i);
+// }
 
-let obj ={
-    name: "Abddul Rahman",
-    role: "Web Developer",
-    company: "Tech Solutions", 
-}
+// let obj ={
+//     name: "Abddul Rahman",
+//     role: "Web Developer",
+//     company: "Tech Solutions", 
+// }
 
-for (const key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) {
-        const element = obj[key];
-        
-    }
-}
+// for (const key in obj) {
+//         const element = obj[key];
+//         console.log(key)
+//     }
+
+//     for (const c of "Abdul Rahman") {
+//         console.log(c);
+//     }
+
+// let i=0;
+// while (i < 6) {
+//     console.log(i);
+//     i++;
+// }
+
+let i=10;
+do {
+    console.log(i);
+    i++;
+} while (i<6);
